2c6ccf2e04d33ee4e7e960c1a3bfaf56c45ba466
token <239573049@qq.com>
2024-02-18 01:09:38 +0800

    增加知识库详情

---
+++ src/Service/FastWiki.Service/Application/Wikis/Queries/WikiDetailsQuery.cs
@@ -0,0 +1,13 @@
+﻿namespace FastWiki.Service.Application.Wikis.Queries;
+
+/// <summary>
+/// 获取知识库详情列表
+/// </summary>
+/// <param name="WikiId"></param>
+/// <param name="Keyword"></param>
+/// <param name="Page"></param>
+/// <param name="PageSize"></param>
+public record WikiDetailsQuery(long WikiId,string? Keyword, int Page, int PageSize) : Query<PaginatedListBase<WikiDetailDto>>()
+{
+    public override PaginatedListBase<WikiDetailDto> Result { get; set; }
+}
---
+++ src/Web/FastWiki.Web.Rcl/Pages/Wikis/WikiInfo.razor.cs
@@ -0,0 +1,60 @@
+﻿using FastWiki.Service.Contracts.Wikis.Dto;
+using Masa.Utils.Models;
+
+namespace FastWiki.Web.Rcl.Pages.Wikis;
+
+public partial class WikiInfo
+{
+    [Parameter] public long Id { get; set; }
+
+    private PaginatedListBase<WikiDetailDto> _wikiDetails = new();
+
+    private int page = 1;
+    private int pageSize = 10;
+
+    private string keyword = "";
+
+    private readonly List<DataTableHeader<WikiDetailDto>> _headers =
+    [
+        new()
+        {
+            Text = "文件名",
+            Sortable = false,
+            Value = nameof(WikiDetailDto.FileName)
+        },
+
+        new()
+        {
+            Text = "索引数量",
+            Sortable = false, Value = nameof(WikiDetailDto.DataCount)
+        },
+
+        new()
+        {
+            Text = "数据类型",
+            Sortable = false, Value = nameof(WikiDetailDto.Type)
+        },
+
+        new()
+        {
+            Text = "创建时间",
+            Sortable = false, Value = nameof(WikiDetailDto.CreationTime)
+        },
+
+        new()
+        {
+            Text = "操作",
+            Sortable = false,
+        }
+    ];
+
+    private async Task LoadData()
+    {
+        _wikiDetails = await WikiService.GetWikiDetailsAsync(Id, keyword, page, pageSize);
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadData();
+    }
+}
