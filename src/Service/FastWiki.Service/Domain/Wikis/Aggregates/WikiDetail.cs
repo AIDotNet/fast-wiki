944cac95d32a683810a7afc45c36e4b50ad5cd76
token <239573049@qq.com>
2024-09-04 16:09:15 +0800

    修复映射异常

--- src/Service/FastWiki.Service/Domain/Wikis/Aggregates/WikiDetail.cs
+++ src/Service/FastWiki.Service/Domain/Wikis/Aggregates/WikiDetail.cs
@@ -19,7 +19,7 @@ public class WikiDetail : Entity<long>, IAuditEntity<long>
         State = WikiQuantizationState.None;
     }
 
-    protected WikiDetail()
+    public WikiDetail()
     {
     }
 
